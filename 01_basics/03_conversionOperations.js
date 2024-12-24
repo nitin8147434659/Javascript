e85b322639d0b3e29416f5ce7a122d42cb08849b
nitin8147434659 <121577313+nitin8147434659@users.noreply.github.com>
2024-12-24 11:00:45 +0000

    Converion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = true
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+//'33' when converted using the Number you will get 33
+//'33abc' => Nan 
+//'true'=> 1
+//'false' => 0
+
+
+let isLoggegIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggegIn)
+
+// console.log(booleanIsLoggedIn);
+
+//1=> true
+//0 => false
+//'' => false
+// 'nitin' => true
+
+let number = 44
+let stringNumber = String(number)
+console.log(stringNumber); // 44
+console.log(typeof stringNumber); // string
+
+
