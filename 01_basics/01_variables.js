14f3d1f5b5a826c5a9ecf7b4c09377d1b07621f2
nitin8147434659 <121577313+nitin8147434659@users.noreply.github.com>
2024-12-24 10:37:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 111111
+let email = 'nitin@gmail.com'
+var accountPassword = '12345'
+accountCity = 'Mysusru'
+
+
+email='nitin222@gmail.com'
+accountPassword= '22222'
+accountCity = "kalaburagi"
+
+console.table([email,accountId,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// code readability is must
+
+console.log("nitin");
+
+let name = 'NitinGuttedar'
+let age = 24
+let isLoggedIn = true
+
+
+console.log(typeof(name)); //string
+console.log(typeof(age)); // number
+console.log(typeof(isLoggedIn)); //boolean
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
+
+
+
+
+
+
+
